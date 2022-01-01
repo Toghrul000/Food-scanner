137eb3b5c597309a5f56bac5d39f94dbfbee13e8
s2428245 <t.garalov@student.utwente.nl>
2022-01-01 17:09:06 +0100

    SOme tests on network json

---
+++ app/src/main/java/com/example/foodscanner/Handler.java
@@ -0,0 +1,73 @@
+package com.example.foodscanner;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class Handler {
+
+    private static final String TAG  = Handler.class.getSimpleName();
+
+
+    public Handler(){
+
+    }
+
+    public String httpServiceCall(String requestUrl){
+        String result = null;
+
+
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStream inputStream = new BufferedInputStream(connection.getInputStream());
+            result = convertResultToString(inputStream);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+
+
+    }
+
+    private String convertResultToString(InputStream inputStream) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String li;
+
+        try {
+            while ((li = bufferedReader.readLine()) != null) {
+                stringBuilder.append('\n');
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return stringBuilder.toString();
+
+
+    }
+}
--- app/src/main/java/com/example/foodscanner/Results.java
+++ app/src/main/java/com/example/foodscanner/Results.java
@@ -1,17 +1,39 @@
 package com.example.foodscanner;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Results extends AppCompatActivity {
 
     Button search ;
     Button home;
+    TextView textView;
+
+    String url = "https://world.openfoodfacts.org/api/v0/product/23073159.json";
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +44,15 @@ public class Results extends AppCompatActivity {
 
         search = findViewById(R.id.scanA);
         home = findViewById(R.id.home);
+
+        textView = findViewById(R.id.textView);
+
+        new GetJSONTask().execute(url);
+
+
+
+
+
 
         search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +70,10 @@ public class Results extends AppCompatActivity {
 
     }
 
+
+
+
+
     public void openScanBar(){
         Intent barScan = new Intent(this, MainActivity.class);
         startActivity(barScan);
@@ -47,6 +82,73 @@ public class Results extends AppCompatActivity {
         Intent home = new Intent(this, HomePage.class);
         startActivity(home);
     }
+
+
+
+
+    class GetJSONTask extends AsyncTask<String, Void, JsonObject> {
+
+        private String name;
+        private String imageUrl;
+        private double sugar;
+        private double carbs;
+        private double fat;
+        private double salt;
+        private double energy;
+        private double sodium;
+
+
+        @Override
+        protected JsonObject doInBackground(String... urls) {
+
+            JsonObject rootJson = new JsonObject();
+            URL jsonURL = null;
+
+            try {
+                jsonURL = new URL(urls[0]);
+            } catch (java.net.MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                assert jsonURL != null;
+                URLConnection request = jsonURL.openConnection();
+                request.connect();
+                JsonElement jsonElement = JsonParser.parseReader(new InputStreamReader((InputStream) request.getContent()));
+                rootJson = jsonElement.getAsJsonObject();
+            } catch(java.io.IOException e) {
+                e.printStackTrace();
+            }
+            JsonObject productJson = rootJson.getAsJsonObject("product");
+
+
+
+            return productJson;
+        }
+
+
+
+        protected void onPostExecute(JsonObject productJson) {
+            name = productJson.get("product_name").getAsString();
+            imageUrl = productJson.get("image_front_url").getAsString();
+            JsonObject nutrients = productJson.getAsJsonObject("nutriments");
+            carbs = nutrients.get("carbohydrates").getAsDouble();
+            sugar = nutrients.get("sugars").getAsDouble();
+            fat = nutrients.get("fat").getAsDouble();
+            salt = nutrients.get("salt").getAsDouble();
+            energy = nutrients.get("energy").getAsDouble();
+            sodium = nutrients.get("sodium").getAsDouble();
+
+            textView.setText(name);
+
+
+
+
+
+
+        }
+    }
+
 
 
 }
